f3c9f02c31ff3024b034692fab67c3313eb6f172
Darren Miller <darren@dmlogic.com>
2019-06-01 09:34:33 +0100

    new files

---
+++ config.sample.py
@@ -0,0 +1,14 @@
+# The earliest we want to turn on
+turnOnEarly = "05:00"
+# The latest we want to be on
+turnOffLate = "23:00"
+# The GPIO control pin
+controlPin = 16
+# The number of seconds after sunset and before sunrise we want to turn on
+minutesOffet = 1800
+# Our latitude
+lat =
+# Our longitude
+lon =
+# Our elevation
+elev = 108
